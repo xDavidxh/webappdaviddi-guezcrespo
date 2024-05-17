ffb4047633edd55bd99cd33da6e2dbd1c95208a2
David <168082296+xDavidxh@users.noreply.github.com>
2024-05-17 11:55:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -78,3 +78,41 @@ function tanca_sessio() {
         }
     }
 }
+window.onload = () => { 
+    let base_de_dades = storage.getItem("base_de_dades");   
+    if(base_de_dades == null) {
+        indexedDB.open("Dades").onupgradeneeded = event => {   
+            event.target.result.createObjectStore("Fotos", {keyPath: "ID", autoIncrement:true}).createIndex("Usuari_index", "Usuari");
+        }    // les fotos es desen a la taula "Fotos"
+        storage.setItem("base_de_dades","ok");
+    }
+    document.getElementById("obturador").addEventListener("change", function() {    // procediment que s'executa quan s'obté el fitxer de la foto realitzada (esdeveniment "change")
+        if(this.files[0] != undefined) {    // instruccions que s'executen només si s'obté algun fitxer (només es processa el primer que es rebi)
+            let canvas = document.getElementById("canvas");    // contenidor on es desa temporalment la imatge
+            let context = canvas.getContext("2d");
+            let imatge = new Image;
+            imatge.src = URL.createObjectURL(this.files[0]);    // es crea la imatge a partir del fitxer
+            imatge.onload = () => {    // procediment que s'executa un cop la imatge s'ha carregat en el contenidor
+                canvas.width = imatge.width;
+                canvas.height = imatge.height;                
+                context.drawImage(imatge,0,0,imatge.width,imatge.height);    // es "dibuixa" la imatge en el canvas
+                document.getElementById("foto").src = canvas.toDataURL("image/jpeg");    // la imatge es mostra en format jpg
+                document.getElementById("icona_camera").style.display = "none";    // s'oculta la icona que hi havia abans de fer la foto
+                document.getElementById("desa").style.display = "unset";    // es mostra el botó per desar la foto
+            }
+        }
+    });
+}
+function desa_foto() {
+    let nou_registre = {    // contingut del nou registre de la base de dades
+        Usuari: usuari,    // nom d'usuari
+        Data: new Date().toLocaleDateString() + " - " + new Date().toLocaleTimeString(),    // data i hora actuals
+        Foto: document.getElementById("foto").src    // foto
+    };
+    indexedDB.open("Dades").onsuccess = event => {   
+        event.target.result.transaction("Fotos", "readwrite").objectStore("Fotos").add(nou_registre).onsuccess = () => {
+            document.getElementById("desa").style.display = "none";
+            alert("La foto s'ha desat correctament.");    
+        };
+    };
+}
