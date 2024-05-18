99444cf7fbbaa7e7f47d9c0da8bb5a728c031c15
David <168082296+xDavidxh@users.noreply.github.com>
2024-05-18 20:43:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -212,3 +212,34 @@ function geoExit(posicio){
         geoID.setLatLng([latitud, longitud]);    // actualització de la posició del marcador d'usuari en el mapa
     }
 }
+async function inicia_video() {
+    const codi_model = "https://teachablemachine.withgoogle.com/models/zYVOfqG15/"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
+    const tmURL = "https://teachablemachine.withgoogle.com/models/" + codi_model;
+    const modelURL = tmURL + "/model.json";
+    const metadataURL = tmURL + "/metadata.json";
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPrediccions = model.getTotalClasses();    // nombre de tipus d'imatges per reconèixer
+    webcam = new tmImage.Webcam(300, 300, true);    // posada en marxa de la webcam
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);    // bucle
+    document.getElementById("icona_video").style.display = "none";    // oculta la icona de la càmera de vídeo
+    document.getElementById("coincidencia").style.display = "flex";    // mostra el text amb la predicció de coincidències
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    prediccions = document.getElementById("prediccions");
+    for (let i = 0; i < maxPrediccions; i++) {
+        prediccions.appendChild(document.createElement("div"));    // es crea un contenidor per a la coincidència de cada tipus d'imatge
+    }
+}
+async function loop() {
+    webcam.update();
+    await prediu();
+    window.requestAnimationFrame(loop);
+}
+async function prediu() {
+    const prediccio = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPrediccions; i++) {
+        const classe = prediccio[i].className + ": " + prediccio[i].probability.toFixed(2);    // es conserven dues xifres decimals
+        prediccions.childNodes[i].innerHTML = classe;
+    }
+}
