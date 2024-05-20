334734c33aa9179ddb2406ce2c758e4fa700c6ad
David <168082296+xDavidxh@users.noreply.github.com>
2024-05-20 13:23:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -104,6 +104,10 @@ window.onload = () => {
             }
         }
     });
+mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
+}).addTo(mapa);    // s'afegeix la capa al mapa
 }
 function desa_foto() {
     let nou_registre = {    // contingut del nou registre de la base de dades
